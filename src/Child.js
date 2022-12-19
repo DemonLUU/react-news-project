e1b29384aa5b508aac6c1e51ac3581424788d003
DemonLUU <76080267+DemonLUU@users.noreply.github.com>
2022-12-19 13:06:31 +0000

    feat: xx

--- src/Child.js
+++ src/Child.js
@@ -2,13 +2,14 @@ import React from "react";
 import style from "./Child.module.scss";
 
 // console.log(style);
-export default function Child() {
+export default function Child () {
   return (
     <div>
       child
       <ul>
         <li className={style.item}>cccccccccc</li>
         <li className={style.item}>hhhhhhhhh</li>
+        2222
       </ul>
     </div>
   );
